1195284fdf617285111b93cd9ec79d0219c60196
thisshouldntbeneeded-glitch <thisshouldntbeneeded@gmail.com>
2026-03-06 10:19:24 +0000

    Update data.js
    fix: combine all BRAND_DATA parts into global array

--- data/data.js
+++ data/data.js
@@ -1,1 +1,8 @@
-var BRAND_DATA = [];
+// Combine all data parts into one BRAND_DATA array
+var BRAND_DATA = []
+  .concat(typeof BRAND_DATA_PARTA !== 'undefined' ? BRAND_DATA_PARTA : [])
+  .concat(typeof BRAND_DATA_PARTB !== 'undefined' ? BRAND_DATA_PARTB : [])
+  .concat(typeof BRAND_DATA_PARTC !== 'undefined' ? BRAND_DATA_PARTC : [])
+  .concat(typeof BRAND_DATA_PARTD !== 'undefined' ? BRAND_DATA_PARTD : [])
+  .concat(typeof BRAND_DATA_PARTE !== 'undefined' ? BRAND_DATA_PARTE : [])
+  .concat(typeof BRAND_DATA_PARTF !== 'undefined' ? BRAND_DATA_PARTF : []);
