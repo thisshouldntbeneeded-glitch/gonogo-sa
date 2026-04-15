0fa8f98f18083c54c1bffc613fe191c5d6315acd
Jeremy <jeremy@gonogo.co.uk>
2026-04-15 15:35:30 +0000

    SEO: Blog posts now in sitemap + full article structured data
    - /api/sitemap.js now fetches published blog posts and includes them
    - blog.html added to sitemap as static page
    - blog-post.html: full SEO meta tags (description, OG, Twitter, canonical)
    - blog-post.html: dynamic Article JSON-LD structured data per post

--- api/sitemap.js
+++ api/sitemap.js
@@ -7,9 +7,10 @@ const SITE_URL = 'https://www.gonogo.co.za';
 
 export default async function handler(req, res) {
   try {
-    const [cats, brands] = await Promise.all([
+    const [cats, brands, blogPosts] = await Promise.all([
       sbGet('categories?select=slug'),
-      sbGet('brands?select=slug,category_slug,last_updated&order=last_updated.desc')
+      sbGet('brands?select=slug,category_slug,last_updated&order=last_updated.desc'),
+      sbGet('blog_posts?status=eq.published&region=eq.za&select=slug,published_at,updated_at&order=published_at.desc')
     ]);
 
     const today = new Date().toISOString().split('T')[0];
@@ -36,6 +37,15 @@ export default async function handler(req, res) {
     for (const brand of brands) {
       const lastmod = brand.last_updated || today;
       xml += url(SITE_URL + '/brand.html?id=' + brand.slug, lastmod, '0.7', 'weekly');
+    }
+
+    // Blog listing
+    xml += url(SITE_URL + '/blog.html', today, '0.7', 'daily');
+
+    // Blog posts
+    for (const post of blogPosts) {
+      const lastmod = (post.updated_at || post.published_at || today).split('T')[0];
+      xml += url(SITE_URL + '/blog-post.html?slug=' + post.slug, lastmod, '0.7', 'weekly');
     }
 
     xml += '</urlset>';
